5bfde6a1956b6d397564bf94e7b0ece2141e027c
Atharv <dude123studios@gmail.com>
2021-06-04 17:04:45 -0700

    Primer Updates

--- 3/primer/src/App.js
+++ 3/primer/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 4,
+        };
+    }
+    isEven(val) {
+        return val % 2 === 0 ? "Even" : "Odd";
+    }
+
+    getClassName(val) {
+        return val % 2 === 0
+            ? "bg-primary text-white text-center p-2 m-1"
+            : "bg-secondary text-white text-center p-2 m-1";
+    }
+
+    handleClick = () => this.setState({ count: this.state.count + 1 });
+
+    render = () => (
+        <table className="table table-striped table-bordered table-sm">
+            <thead className="bg-info text-white">
+                <tr>
+                    <th>Value</th>
+                    <th>Even?</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{this.state.count}</td>
+                    <td>{this.isEven(this.state.count)}</td>
+                </tr>
+            </tbody>
+            <tfoot className="text-center">
+                <tr colSpan="2">
+                    <button
+                        className="btn btn-info m-2"
+                        onClick={this.handleClick}
+                    >
+                        Click Me!
+                    </button>
+                </tr>
+            </tfoot>
+        </table>
+    );
 }
-
-export default App;
